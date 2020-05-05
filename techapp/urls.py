b1c860b5337d61e039542d519f69e11082f788d6
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-06 02:54:06 +0900

    検索フォームの側を作成

--- techapp/urls.py
+++ techapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "techapp"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('search', views.search, name='search'),
 ]
--- techapp/views.py
+++ techapp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from django.views.generic import CreateView, DeleteView, UpdateView, ListView, TemplateView
 
+
+def search(request):
+    params = {
+    }
+
+    return render(request, 'techapp/index.html', params)
+
 class IndexView(TemplateView):
     template_name = "techapp/index.html"
 
