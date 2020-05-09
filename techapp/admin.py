13b37637ee546f2feead4f1959d869a518018e4d
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-09 13:00:26 +0900

    管理画面の表示を変更

--- techapp/admin.py
+++ techapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Pref
+from .models import Category, Pref, Review
 # Register your models here.
 
 @admin.register(Category)
@@ -14,3 +14,9 @@ class PrefAdmin(admin.ModelAdmin):
     list_display = ('pref', 'name')
     list_display_links = ('pref',)
     list_editable = ('name',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('shop_id', 'shop_name', 'user', 'score',)
+    list_display_links = ('shop_name',)
+    list_editable = ('score',)
